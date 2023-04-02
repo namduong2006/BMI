a0928fd02753c1993f263c6691275afa98341c2e
namduong2006 <129411334+namduong2006@users.noreply.github.com>
2023-04-02 21:09:04 +0700

    Add project files.

---
+++ BMI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            float nang, cao, bmi;
+            Console.Write("Cân nặng của bạn (Kg) :");
+            nang= float.Parse(Console.ReadLine());
+            Console.Write("Chiều cao của bạn (m) :");
+            cao= float.Parse(Console.ReadLine());
+            bmi = nang / (cao * cao);
+            Console.WriteLine("Chỉ số BMI của bạn là : "+bmi);
+            if (bmi<18)
+                Console.WriteLine("Gầy");
+            else if ( bmi<25)
+                Console.WriteLine("Bình thường");
+            else if(bmi<30)
+                Console.WriteLine("Thừa cân");
+            else
+                Console.WriteLine("Béo phì");
+            Console.ReadLine();
+        }
+    }
+}
